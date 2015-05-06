1c4d8f115aa856cbd15a066bcc0df2e8aff75d51
jordan & bob <student@epicodus.com>
2015-05-06 16:56:22 -0700

    Add more code

--- app.rb
+++ app.rb
@@ -8,5 +8,33 @@ require("pg")
 DB = PG.connect({:dbname => "train_db"})
 
 get("/") do
+  @trains = Train.all()
+  @cities = City.all()
   erb(:index)
+end
+
+get("/cities") do
+  @cities = City.all()
+  erb(:cities)
+end
+
+get("/trains") do
+  @trains = Train.all()
+  erb(:trains)
+end
+
+post("/cities") do
+  name = params.fetch("name")
+  city = City.new({:name => name, :id => nil})
+  city.save()
+  @cities = City.all()
+  erb(:cities)
+end
+
+post("/trains") do
+  name = params.fetch("name")
+  train = Train.new({:name => name, :id => nil})
+  train.save()
+  @trains = Train.all()
+  erb(:trains)
 end
