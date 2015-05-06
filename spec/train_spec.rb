93fdff6091472f89a5300ce1455ac196cbc5b36c
jordan & bob <student@epicodus.com>
2015-05-06 13:54:59 -0700

    Return the id of the train

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -9,4 +9,13 @@ describe(Train) do
       expect(test_train.name()).to(eq("Red Train"))
     end
   end
+
+  describe('#id') do
+    it("returns the id of the train") do
+      test_train = Train.new({:name => "Red Train", :id => 1})
+      expect(test_train.id()).to(eq(1))
+    end
+
+  end
+
 end
